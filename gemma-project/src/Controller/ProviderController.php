d3e1becc80f47b64e3b1b8e51c4f7f5822668d6e
Gemma Goitia <gemma.goitia@estudiants.urv.cat>
2024-05-07 19:09:43 +0200

    Commit Inicial

---
+++ gemma-project/src/Controller/ProviderController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProviderController extends AbstractController
+{
+    /**
+     * @Route("/provider", name="app_provider")
+     */
+    public function index(): Response
+    {
+        return $this->render('provider/index.html.twig', [
+            'controller_name' => 'ProviderController',
+        ]);
+    }
+}
---
+++ gemma-project/src/Entity/Provider.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProviderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProviderRepository::class)
+ */
+class Provider
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+
+    // Les dades que necesitarem guardar són: nom, correu electónic, telefon, tipo de proveidor i activitat
+    // Heurem de tenir constància de quan s'ha introduit el proveidor i quan s'ha modificat
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $activity;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;    // Data de creació
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;    // Data de modificació
+
+
+    // Constructor
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+        $this->updated_at = new \DateTime();
+        $this->activity = true; // Posarem que el proveidor està actiu per defecte, ja si ha de ser fals serà modificat a la vista d'edició
+    }
+
+    // Getters i Setters de cada una de les propietats
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function getActivity(): ?bool
+    {
+        return $this->activity;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function setActivity(bool $activity): self
+    {
+        $this->activity = $activity;
+
+        return $this;
+    }
+
+    // Funció per a saber quan s'ha introduit el proveidor
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    // Funció per a saber quan s'ha modificat el proveidor
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    // Funció per a que s'actualitzi la data de modificació
+    // Només haurem de modificar la data de modificació de les dades. La de creació no es tocarà
+    public function setUpdatedAt(): self
+    {
+        $this->updated_at = new \DateTime();
+
+        return $this;
+    }
+}
